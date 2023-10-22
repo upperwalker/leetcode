7cc839defbbf1e3568d3b92ce4c0351833c6e7b7
Anton Bordachev <42139148+upperwalker@users.noreply.github.com>
2023-10-22 19:23:49 +0200

    Update find-the-difference.ts

--- find-the-difference.ts
+++ find-the-difference.ts
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/find-the-difference/description/
+
 function findTheDifference(s: string, t: string): string {
    return String.fromCharCode(Array.from((s + t)).reduce((acc,char) => acc ^ char.charCodeAt(0), 0))
 };
